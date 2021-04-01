c40f190d33fadd8a035382e16a4ebb8e107676cb
hhhallan <allanstm76@gmail.com>
2021-04-01 19:25:32 +0200

    qlq commentraires

--- App/Controller/UserController.php
+++ App/Controller/UserController.php
@@ -15,6 +15,7 @@ class UserController extends Controller
         $this->userModelPro = new UserModelPro();
     }
 
+    // FONCTIONS UTILES POUR TOUS
     public function showJson()
     {
         $data = $this->userModelPro->ReadOne($data['id']);
@@ -34,77 +35,57 @@ class UserController extends Controller
         ]);
     }
 
-    public function signup_parent($data)
+    public function isLogged()
     {
-        $success = false;
-        $errors = array();
-        if (isset($data['nom']) && isset($data['prenom']) && isset($data['tel_portable']) && isset($data['tel_fixe']) && isset($data['email']) && isset($data['mot_de_passe'])) {
-
-            $user = $this->encodeChars($data);
-
-            /*    CLEAN XSS     */
-            $name = $this->cleanXss($user['nom']);
-            $firstname = $this->cleanXss($user['prenom']);
-            $tel = $this->cleanXss($user['tel_portable']);
-            $tel2 = $this->cleanXss($user['tel_fixe']);
-            $email = $this->cleanXss($user['email']);
-            $password = $this->cleanXss($user['mot_de_passe']);
-            $password_confirm = $this->cleanXss($user['password_confirm']);
-
-            /*    VERIFICATION TEXTE     */
-            $errors = $this->validationText($errors, $name, 'nom', 2, 50);
-            $errors = $this->validationText($errors, $firstname, 'prenom', 2, 50);
-
-            /*    VERIFICATION EMAIL     */
-            if (!empty($email)) {
-                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
-                    $errors['email'] = 'Veuillez renseigner un e-mail valide.';
-                } else {
-                    $verifEmail = $this->userModelParent->getIdByMail($email);
-                    if (!empty($verifEmail)) {
-                        $errors['email'] = 'Cet e-mail est déjà utilisé.';
+        if (!empty($_SESSION['user'])) {
+            if (!empty($_SESSION['user']['id']) && is_numeric($_SESSION['user']['id'])) {
+                if (!empty($_SESSION['user']['email'])) {
+                    if (!empty($_SESSION['user']['ip']) && $_SESSION['user']['ip'] == $_SERVER['REMOTE_ADDR']) {
+                        return true;
                     }
                 }
-            } else {
-                $errors['email'] = 'Veuillez renseigner un e-mail.';
-            }
-
-            /*    VERIFICATION PASSWORD     */
-            if (!empty($password) && !empty($password_confirm)) {
-
-                if ($password != $password_confirm) {
-                    $errors['password_confirm'] = 'Veuillez renseigner des mots de passe identiques.';
-                } elseif (mb_strlen($password) < 6) {
-                    $errors['mot_de_passe'] = 'Minimum 6 caractères';
-                }
-            } else {
-                $errors['mot_de_passe'] = 'Veuillez renseigner vos mots de passe.';
-            }
-            /*    VERIFICATION TELEPHONE     */
-
-            /*  CONVERTIR ADRESSE => COORDONNEES   */
-
-            /*   0 ERREUR   */
-            if (count($errors) === 0) {
-                $success = true;
-
-                // insertion BDD
-                $hashPassword = password_hash($password, PASSWORD_DEFAULT);
-                $user['mot_de_passe'] = $hashPassword;
-
-                unset($user['password_confirm']);
-
-
-                $this->userModelParent->create($user);
-                header('Location: index.php?page=login_parent');
             }
         }
-        $this->render("auth.signup_parent", [
-            'errors' => $errors
-        ]);
-
+        return false;
     }
 
+    public function isLoggedPro()
+    {
+        if (!empty($_SESSION['user'])) {
+            if (!empty($_SESSION['user']['id']) && is_numeric($_SESSION['user']['id'])) {
+                if (!empty($_SESSION['user']['email'])) {
+                    if ($_SESSION['user']['role'] == 'creche' || $_SESSION['user']['role'] == 'assistantemater' || $_SESSION['user']['role'] == 'babysitter')
+                        if (!empty($_SESSION['user']['ip']) && $_SESSION['user']['ip'] == $_SERVER['REMOTE_ADDR']) {
+                            return true;
+                        }
+                }
+            }
+        }
+        return false;
+    }
+
+    public function isLoggedParent()
+    {
+        if (!empty($_SESSION['user'])) {
+            if (!empty($_SESSION['user']['id']) && is_numeric($_SESSION['user']['id'])) {
+                if (!empty($_SESSION['user']['email'])) {
+                    if ($_SESSION['user']['role'] == 'parent')
+                        if (!empty($_SESSION['user']['ip']) && $_SESSION['user']['ip'] == $_SERVER['REMOTE_ADDR']) {
+                            return true;
+                        }
+                }
+            }
+        }
+        return false;
+    }
+
+    public function logout()
+    {
+        session_destroy();
+        header("Location:index.php");
+    }
+
+    // FONCTIONS POUR LES PRO
     public function signup_pro($data)
     {
 
@@ -224,6 +205,83 @@ class UserController extends Controller
 
     }
 
+    public function getAllPro()
+    {
+        return $this->userModelPro->ReadAll();
+    }
+
+    // FONCTIONS POUR LES PARENTS
+    public function signup_parent($data)
+    {
+        $success = false;
+        $errors = array();
+        if (isset($data['nom']) && isset($data['prenom']) && isset($data['tel_portable']) && isset($data['tel_fixe']) && isset($data['email']) && isset($data['mot_de_passe'])) {
+
+            $user = $this->encodeChars($data);
+
+            /*    CLEAN XSS     */
+            $name = $this->cleanXss($user['nom']);
+            $firstname = $this->cleanXss($user['prenom']);
+            $tel = $this->cleanXss($user['tel_portable']);
+            $tel2 = $this->cleanXss($user['tel_fixe']);
+            $email = $this->cleanXss($user['email']);
+            $password = $this->cleanXss($user['mot_de_passe']);
+            $password_confirm = $this->cleanXss($user['password_confirm']);
+
+            /*    VERIFICATION TEXTE     */
+            $errors = $this->validationText($errors, $name, 'nom', 2, 50);
+            $errors = $this->validationText($errors, $firstname, 'prenom', 2, 50);
+
+            /*    VERIFICATION EMAIL     */
+            if (!empty($email)) {
+                if (!filter_var($email, FILTER_VALIDATE_EMAIL)) {
+                    $errors['email'] = 'Veuillez renseigner un e-mail valide.';
+                } else {
+                    $verifEmail = $this->userModelParent->getIdByMail($email);
+                    if (!empty($verifEmail)) {
+                        $errors['email'] = 'Cet e-mail est déjà utilisé.';
+                    }
+                }
+            } else {
+                $errors['email'] = 'Veuillez renseigner un e-mail.';
+            }
+
+            /*    VERIFICATION PASSWORD     */
+            if (!empty($password) && !empty($password_confirm)) {
+
+                if ($password != $password_confirm) {
+                    $errors['password_confirm'] = 'Veuillez renseigner des mots de passe identiques.';
+                } elseif (mb_strlen($password) < 6) {
+                    $errors['mot_de_passe'] = 'Minimum 6 caractères';
+                }
+            } else {
+                $errors['mot_de_passe'] = 'Veuillez renseigner vos mots de passe.';
+            }
+            /*    VERIFICATION TELEPHONE     */
+
+            /*  CONVERTIR ADRESSE => COORDONNEES   */
+
+            /*   0 ERREUR   */
+            if (count($errors) === 0) {
+                $success = true;
+
+                // insertion BDD
+                $hashPassword = password_hash($password, PASSWORD_DEFAULT);
+                $user['mot_de_passe'] = $hashPassword;
+
+                unset($user['password_confirm']);
+
+
+                $this->userModelParent->create($user);
+                header('Location: index.php?page=login_parent');
+            }
+        }
+        $this->render("auth.signup_parent", [
+            'errors' => $errors
+        ]);
+
+    }
+
     public function login_parent($data)
     {
         $errors = array();
@@ -265,63 +323,8 @@ class UserController extends Controller
 
     }
 
-    public function logout()
-    {
-        session_destroy();
-        header("Location:index.php");
-    }
-
-    public function getAllPro()
-    {
-        return $this->userModelPro->ReadAll();
-    }
-
     public function getAllParent()
     {
         return $this->userModelParent->ReadAll();
-    }
-
-    public function isLogged()
-    {
-        if (!empty($_SESSION['user'])) {
-            if (!empty($_SESSION['user']['id']) && is_numeric($_SESSION['user']['id'])) {
-                if (!empty($_SESSION['user']['email'])) {
-                    if (!empty($_SESSION['user']['ip']) && $_SESSION['user']['ip'] == $_SERVER['REMOTE_ADDR']) {
-                        return true;
-                    }
-                }
-            }
-        }
-        return false;
-    }
-
-    public function isLoggedPro()
-    {
-        if (!empty($_SESSION['user'])) {
-            if (!empty($_SESSION['user']['id']) && is_numeric($_SESSION['user']['id'])) {
-                if (!empty($_SESSION['user']['email'])) {
-                    if ($_SESSION['user']['role'] == 'creche' || $_SESSION['user']['role'] == 'assistantemater' || $_SESSION['user']['role'] == 'babysitter')
-                        if (!empty($_SESSION['user']['ip']) && $_SESSION['user']['ip'] == $_SERVER['REMOTE_ADDR']) {
-                            return true;
-                        }
-                }
-            }
-        }
-        return false;
-    }
-
-    public function isLoggedParent()
-    {
-        if (!empty($_SESSION['user'])) {
-            if (!empty($_SESSION['user']['id']) && is_numeric($_SESSION['user']['id'])) {
-                if (!empty($_SESSION['user']['email'])) {
-                    if ($_SESSION['user']['role'] == 'parent')
-                        if (!empty($_SESSION['user']['ip']) && $_SESSION['user']['ip'] == $_SERVER['REMOTE_ADDR']) {
-                            return true;
-                        }
-                }
-            }
-        }
-        return false;
     }
 }
